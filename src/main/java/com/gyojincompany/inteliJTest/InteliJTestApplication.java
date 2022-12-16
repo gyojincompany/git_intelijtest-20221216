627dc8ad0488a150873149f86ec5ed07e126c2cf
gyojincompany <luckyred@gmail.com>
2022-12-16 16:46:28 +0900

    test

---
+++ src/main/java/com/gyojincompany/inteliJTest/InteliJTestApplication.java
@@ -0,0 +1,13 @@
+package com.gyojincompany.inteliJTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InteliJTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InteliJTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gyojincompany/inteliJTest/InteliJTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gyojincompany.inteliJTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InteliJTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
